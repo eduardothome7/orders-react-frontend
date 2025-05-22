e1adcc8d4ac5808689dd5244cbae6046ff4c0053
Eduardo Cannini Thomé <eduardocanninithome@gmail.com>
2025-05-22 15:10:53 -0300

    SETUP Projeto

---
+++ src/pusher.js
@@ -0,0 +1,9 @@
+// src/pusher.js
+import Pusher from 'pusher-js';
+
+const pusher = new Pusher(import.meta.env.VITE_PUSHER_KEY, {
+  cluster: import.meta.env.VITE_PUSHER_CLUSTER,
+  encrypted: true,
+});
+  
+export default pusher;
