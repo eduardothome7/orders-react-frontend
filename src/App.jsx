a4853eabbc03a2f05454678b994c6816806698bf
Eduardo Cannini Thomé <eduardocanninithome@gmail.com>
2025-05-24 14:57:23 -0300

    toast e refatoração de calendário

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,31 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Rooms from './components/Rooms'
 import RoomAgenda from './components/RoomAgenda'
 import './App.css'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Rooms />} />
-        <Route path="/rooms/:id" element={<RoomAgenda />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+        <ToastContainer 
+          position="top-right" 
+          autoClose={3000} 
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          pauseOnHover
+          draggable
+          theme="light" // ou 'dark'
+      />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Rooms />} />
+          <Route path="/rooms/:id" element={<RoomAgenda />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
--- src/components/RoomAgenda.jsx
+++ src/components/RoomAgenda.jsx
@@ -1,15 +1,100 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export default function RoomAgenda() {
   const { id } = useParams();
   const [room, setRoom] = useState(null);
+  const [dateEventTarget, setDateEventTarget] = useState(false)
+  const [gridTarget, setGridTarget] = useState(false)
+  const [eventTarget, setEventTarget] = useState(false)
+  const [showEventModal, setShowEventModal] = useState(false)
+  const [newEventModal, setNewEventModal] = useState(false)
+
+  function openNewEventModal(day, grid) {
+    setNewEventModal(true)
+    setDateEventTarget(day)
+    setGridTarget(grid)
+  }
+
+  const showEvent = (event) => {
+    setShowEventModal(true)
+    setEventTarget(event)
+  }
+
+  const closeModal = () => {
+    setGridTarget(null)
+    setDateEventTarget(null)
+    setShowEventModal(false)
+    setNewEventModal(false)
+  }
+
+  const cancelEvent = async () => {
+    try {
+
+      const response = await fetch(`http://localhost:8000/events/${eventTarget.id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        const error = await response.json();
+        console.error('Erro ao deletar:', error);
+        return;
+      }
+
+      fetch(`http://localhost:8000/rooms/${id}`)
+        .then(res => res.json())
+        .then(data => {
+          setRoom(data)
+      })    
+
+      toast.success("Evento cancelado com sucesso!");
+
+      closeModal()
+
+    } catch (err) {
+      alert(err)
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+  
+    const formData = new FormData(e.target)
+    const data = Object.fromEntries(formData.entries())
+
+    try {
+      await fetch(`http://localhost:8000/events`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+
+      fetch(`http://localhost:8000/rooms/${id}`)
+        .then(res => res.json())
+        .then(data => {
+          setRoom(data)
+        })    
+
+      toast.success("Evento criado com sucesso!");
+
+      closeModal()
+
+    } catch (err) {
+      alert(err)
+    }
+  }
 
   useEffect(() => {
     fetch(`http://localhost:8000/rooms/${id}`)
       .then(res => res.json())
       .then(data => {
-        console.log(data)
         setRoom(data)
       });
   }, [id]);
@@ -48,8 +133,14 @@ export default function RoomAgenda() {
                 <td>{grid.start_at}</td>
                 {
                   grid.days.map(day => (                    
-                    <td className={day.event ? 'gridItem checked' : 'gridItem'}>  
-                      {day.event ? <strong>Cliente 001</strong> : <small>Livre</small>}
+                    <td className={day.event ? 'gridItem checked' : 'gridItem'} onClick={() => {
+                      if (day.event) {
+                        showEvent(day.event)
+                      } else {
+                        openNewEventModal(day, grid)
+                      }
+                    }}>
+                      {day.event ? <small><strong>#{day.event.id}</strong></small> : <small>Livre</small>}
                     </td>
                   ))
                 }
@@ -58,6 +149,37 @@ export default function RoomAgenda() {
           }
         </tbody>
       </table>
+
+      {newEventModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <h3>Novo Agendamento</h3>
+            <p>{ dateEventTarget.date }</p>
+            <p>{ gridTarget.start_at } até { gridTarget.end_at }</p>
+            <form onSubmit={handleSubmit}>
+
+              <input type="hidden" name="room_id" value={ id } />
+              <input type="hidden" name="pos" value={ dateEventTarget.pos } />
+              <input type="hidden" name="day" value={ dateEventTarget.dayYear } />
+              <input type="hidden" name="year" value={ dateEventTarget.year } />
+
+              <button className="transparent-button" onClick={closeModal}>Cancelar </button>
+              <button type="submit">Salvar</button>
+            </form>
+          </div>
+        </div>
+      )}
+
+      {showEventModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <h3>Agendamento #{eventTarget.id}</h3>
+            <p></p>
+            <button className="transparent-button" onClick={closeModal}>Fechar</button>
+            <button className="danger" onClick={cancelEvent}>Cancelar</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
